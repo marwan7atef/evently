271afa1b89a6689e1bd4c7ad3307132cb1e8ee6c
Marwan Atef <atefmarwan60@gmail.com>
2025-01-25 20:14:25 +0200

    firebase integrated

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:evently/app_theam.dart';
 import 'package:evently/auth/login_Screen.dart';
 import 'package:evently/creat_event.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'Home/home_screen.dart';
 import 'auth/register_screen.dart';
 
-void main() {
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(EventlyApp());
 }
 class EventlyApp extends StatelessWidget {
